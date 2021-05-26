81c4873ad09864f15d3120ca16587ac1b0304cd7
Richie <TheMadMaker2@protonmail.com>
2021-05-26 12:31:54 -0400

    convert spaces to tabs

--- Scrubing.sh
+++ Scrubing.sh
@@ -11,32 +11,32 @@ sleep 5
 #Gets spesified ZFS Pool status
 zpool status -v $temp1 > $temp2
 while $X eq 1; do
-  sleep $amount
-  if "$(grep 'scan: scrub in progress' $temp2)" eq 0; then
-    echo "test"
-    elif "$(grep 'scan: scrub in progress' $temp2)" eq 1; then
-    x=0
-    cp temp2 to permnet file
-  fi
+	sleep $amount
+	if "$(grep 'scan: scrub in progress' $temp2)" eq 0; then
+		echo "test"
+		elif "$(grep 'scan: scrub in progress' $temp2)" eq 1; then
+		x=0
+		cp temp2 to permnet file
+	fi
 done
 
 
 pool: Media
  state: ONLINE
-  scan: scrub repaired 0B in 0 days 00:04:03 with 0 errors on Tue May  4 12:48:15 2021
+	scan: scrub repaired 0B in 0 days 00:04:03 with 0 errors on Tue May  4 12:48:15 2021
 config:
 
-        NAME                                           STATE     READ WRITE CKSUM
-        Media                                          ONLINE       0     0     0
-          mirror-0                                     ONLINE       0     0     0
-            nvme-INTEL_SSDPEKNW010T8_BTNH0015088S1P0B  ONLINE       0     0     0
-            nvme-INTEL_SSDPEKNW010T8_BTNH005407HX1P0B  ONLINE       0     0     0
+				NAME                                           STATE     READ WRITE CKSUM
+				Media                                          ONLINE       0     0     0
+					mirror-0                                     ONLINE       0     0     0
+						nvme-INTEL_SSDPEKNW010T8_BTNH0015088S1P0B  ONLINE       0     0     0
+						nvme-INTEL_SSDPEKNW010T8_BTNH005407HX1P0B  ONLINE       0     0     0
 
 errors: No known data errors
 
 sudo zpool status Storage 
-  pool: Storage
+	pool: Storage
  state: ONLINE
-  scan: resilvered 5.52G in 0 days 00:10:31 with 0 errors on Tue Apr 27 14:50:50 2021
+	scan: resilvered 5.52G in 0 days 00:10:31 with 0 errors on Tue Apr 27 14:50:50 2021
 config:
 
