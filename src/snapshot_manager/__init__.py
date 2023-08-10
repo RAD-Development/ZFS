ddd4f0669ec2e800cfc4b101fd4908dbd0814a51
Richie Cahill <richie@tmmworkshop.com>
2023-08-09 21:57:35 -0400

    Built snapshot_manager

---
+++ src/snapshot_manager/__init__.py
@@ -0,0 +1,3 @@
+from .snapshot_manager import load_config_data, get_snapshots, create_snapshots, manage_snapshots, main
+
+
---
+++ src/snapshot_manager/__main__.py
@@ -0,0 +1,4 @@
+from snapshot_manager import main
+
+if __name__ == "__main__":
+    main()
